aaf8bf9f7a972bdbc8ba6d532ac17a8c0deaeaa5
eldor2003 <qulsoatoveldor@gmail.com>
2024-03-19 18:03:40 +0500

    partner_section

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,79 @@
-$(document).ready(function(){
-    
-    // hamburger
-    $(".hamurger_menu").on('click',function(e){
-        e.preventDefault();
-        $(".header_menus").addClass("active")
-    })
-    
-    $(".close").on('click',function(e){
-        e.preventDefault();
-        $(".header_menus").removeClass("active")
-    })
+$(document).ready(function () {
+	// hamburger
+	$(".hamurger_menu").on("click", function (e) {
+		e.preventDefault();
+		$(".header_menus").addClass("active");
+	});
 
+	$(".close").on("click", function (e) {
+		e.preventDefault();
+		$(".header_menus").removeClass("active");
+	});
 
-    // 
+	// header menus top
+	function moveLine() {
+		const line = document.querySelector(".line"); // Line elementini tanlash
+		const links = document.querySelectorAll(".header_menus li"); // Barcha linklarni tanlash
+		links.forEach((link, index) => {
+			link.addEventListener("click", function () {
+				console.log(index + 1);
+				switch (index + 1) {
+					case 1:
+						line.style.width = "94%";
+						break;
+					case 2:
+						line.style.width = "77%";
+						break;
+					case 3:
+						line.style.width = "53%";
+						break;
+					case 4:
+						line.style.width = "37%";
+						break;
+					case 5:
+						line.style.width = "18%";
+						break;
+					case 6:
+						line.style.width = "0%";
+						break;
+					default:
+						line.style.width = "94%";
+						break;
+				}
+			});
+		});
+	}
+	moveLine();
+
+	// proektor swiper
+	const proektor_swiper = new Swiper(".proektor_swiper", {
+		// Default parameters
+		loop: true,
+		// direction: "horizontal",
+		direction: "vertical",
+		grabCursor: true,
+		// autoplay: {
+		// 	delay: 500,
+		// },
+		// allowTouchMove: false,
+		// Default parameters
+		pagination: {
+			el: ".pagination",
+			clickable: true,
+		},
+	});
+
+	// description tab
+	$(".tab_content:first").show();
+	$(".tab_desc_item:first").show();
+	$(".tab_navigation li").click(function (event) {
+		event.preventDefault();
+		index = $(this).index();
+		$(".tab_navigation li").removeClass("active");
+		$(this).addClass("active");
+		$(".tab_content").hide();
+		$(".tab_desc_item").hide();
+		$(".tab_content").eq(index).show();
+		$(".tab_desc_item").eq(index).show();
+	});
 });
